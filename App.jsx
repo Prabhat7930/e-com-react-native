6046f26fc583d3c4f4f84f9c007892eca74096b4
Prabhat <prabhat119.dev@gmail.com>
2024-10-20 17:15:47 +0530

    iconography in tab bar

--- App.jsx
+++ App.jsx
@@ -5,6 +5,9 @@ import { HomeScreen } from './src/screens/HomeScreen';
 import { ReorderScreen } from './src/screens/ReorderScreen';
 import { CartScreen } from './src/screens/CartScreen';
 import { ProfileScreen } from './src/screens/ProfileScreen';
+import EntypoIcon from 'react-native-vector-icons/Entypo';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 const Tab = createBottomTabNavigator();
@@ -12,11 +15,37 @@ const Tab = createBottomTabNavigator();
 const App = () => {
   return(
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen}/>
-        <Tab.Screen name="Reorder" component={ReorderScreen}/>
-        <Tab.Screen name="Cart" component={CartScreen}/>
-        <Tab.Screen name="Profile" component={ProfileScreen}/>
+      <Tab.Navigator screenOptions={{
+        headerShown:false,
+      }}>
+        <Tab.Screen name="Home" component={HomeScreen} options={{
+          tabBarIcon: () => {
+            return(
+              <EntypoIcon name={'home'} size={25} color={'#000000'} />
+            );
+          },
+        }}/>
+        <Tab.Screen name="Reorder" component={ReorderScreen} options={{
+          tabBarIcon: () => {
+            return(
+              <FontAwesomeIcon name={'reorder'} size={25} color={'#000000'} />
+            );
+          },
+        }}/>
+        <Tab.Screen name="Cart" component={CartScreen} options={{
+          tabBarIcon: () => {
+            return (
+              <EntypoIcon name={'shopping-cart'} size={25} color={'#000000'} />
+            );
+          },
+        }}/>
+        <Tab.Screen name="Profile" component={ProfileScreen} options={{
+          tabBarIcon: () => {
+            return(
+              <MaterialIcon name={'account'} size={25} color={'#000000'} />
+            );
+          },
+        }}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export const HomeScreen = () => {
     return (
       <View>
-        <Text> HomeScreen </Text>
+        <Text > HomeScreen </Text>
+        <Icon name={'facebook'} size={25} color={'#000000'}/>
       </View>
     );
 };
