62f133087b9fe9edfec99c2efd98c254365032a7
Prabhat <prabhat119.dev@gmail.com>
2024-10-20 18:50:14 +0530

    header component and search bar

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { Header } from '../components/Header';
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
 
 export const HomeScreen = () => {
     return (
@@ -10,6 +11,13 @@ export const HomeScreen = () => {
         style={styles.container}
       >
         <Header />
+        <Text style={styles.headerMessage}>Match Your Style</Text>
+        <View style={styles.searchContainer}>
+          <View style={styles.iconContainer}>
+            <AntDesignIcon name={'search1'} size={25} color={'#C0C0C0'} />
+          </View>
+          <TextInput style={styles.searchInput} placeholder="Search" placeholderTextColor={'#C0C0C0'} cursorColor={'ED96E6'}/>
+        </View>
       </LinearGradient>
     );
 };
@@ -18,5 +26,26 @@ const styles = StyleSheet.create({
   container:{
     flex: 1,
     padding: 20,
+  },
+  headerMessage: {
+    fontSize: 28,
+    fontWeight:'400',
+    color: '#000000',
+    marginVertical: 24,
+  },
+  searchContainer: {
+    backgroundColor: '#FFFFFF',
+    height: 48,
+    borderRadius: 12,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconContainer: {
+    marginHorizontal: 10,
+  },
+  searchInput: {
+    flex: 1,
+    color: '#000000',
   },
 });
